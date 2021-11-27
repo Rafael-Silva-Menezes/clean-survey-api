eea3a00ffd2e34eeaf1b20d985e2dfc94883dd61
Rafael Silva Menezes <rafasilva320.rs@gmail.com>
2021-11-27 13:31:07 -0300

    chore: add paths for jest ignore in jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,12 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  coverageProvider:'babel',
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.ts'
-  ],
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+     '!**/protocols/**',
+      '!**/test/**'
+],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
