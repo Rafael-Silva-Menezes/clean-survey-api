0aff538d1aa0b7f76b730a35d7ec4b5ced330313
Rafael Silva Menezes <rafasilva320.rs@gmail.com>
2021-11-29 16:16:54 -0300

    test: ensure BcryptAdpater return a hash on sucess

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,6 +1,12 @@
 import { BcryptAdpater } from './bcrypt-adapter';
 import bcrypt from 'bcrypt';
 
+jest.mock('bcrypt', () => ({
+  async hash(): Promise<string> {
+    return new Promise(resolve => resolve('hash'));
+  },
+}));
+
 const makeSut = (salt: number): BcryptAdpater => {
   return new BcryptAdpater(salt);
 };
@@ -11,5 +17,12 @@ describe('Bcrypt Adapter', () => {
     const hashSpy = jest.spyOn(bcrypt, 'hash');
     await sut.encrypt('any_value');
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt);
+  });
+
+  test('Shoul return a hash on sucess', async () => {
+    const salt = 12;
+    const sut = makeSut(salt);
+    const hash = await sut.encrypt('any_value');
+    expect(hash).toBe('hash');
   });
 });
