9eac00da309696061b1ba7bb7de0df3e70a55669
Rafael Silva Menezes <rafasilva320.rs@gmail.com>
2022-01-10 17:31:21 -0300

    chore: remove main

--- src/main/config/env.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  mongoUrl:
-    process.env.MONGO_URL ||
-    'mongodb://root:o9wsQKZ1B048a1rzi2bVT757zppk4le0Ma@10.167.72.47:27017/svx3_dev?authSource=admin',
-  port: process.env.PORT || 5050,
-};
