c83bfa6cb3ddecb5c1010f2f8ecd66e69934d1b0
Rafael Silva Menezes <rafasilva320.rs@gmail.com>
2022-01-22 06:58:56 -0300

    feat: ensure requests parse body as json

---
+++ src/main/middlewares/body-parser.test.ts
@@ -0,0 +1,14 @@
+import httpRequest from 'supertest';
+import app from '../config/app';
+
+describe('Body Parser Middleware', () => {
+  test('', async () => {
+    app.post('/test_body_parser', (request, response) => {
+      response.send(request.body);
+    });
+    await httpRequest(app)
+      .post('/test_body_parser')
+      .send({ name: 'John Doe' })
+      .expect({ name: 'John Doe' });
+  });
+});
