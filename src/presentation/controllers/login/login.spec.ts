ad842005f5f8ac7d2fadd6355422c4b041d05b3a
Rafael Silva Menezes <rafasilva320.rs@gmail.com>
2022-01-23 18:13:44 -0300

    refactor: move sut creation to a factory helper method

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -3,13 +3,19 @@ import { badRequest } from '../../helpers';
 import { HttpRequest, HttpResponse } from '../../protocols';
 import { LoginController } from './login';
 
-const makeSut = (): LoginController => {
-  return new LoginController();
+interface SutTypes {
+  sut: LoginController;
+}
+const makeSut = (): SutTypes => {
+  const sut = new LoginController();
+  return {
+    sut,
+  };
 };
 
 describe('Login Controller', () => {
   test('Should return 400 if no email is provided', async () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     const httpRequest: HttpRequest = {
       body: {
         password: 'any_password',
@@ -20,7 +26,7 @@ describe('Login Controller', () => {
   });
 
   test('Should return 400 if no password is provided', async () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     const httpRequest: HttpRequest = {
       body: {
         email: 'any_email@mail.com',
