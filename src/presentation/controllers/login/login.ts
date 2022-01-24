1ce59afcd41113a88be1f1558f13293e6e2abcd7
Rafael Silva Menezes <rafasilva320.rs@gmail.com>
2022-01-24 12:25:19 -0300

    feat: ensure LoginController calls Authentication with correct values

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,3 +1,4 @@
+import { Authentication } from '../../../domain/usecases/authentication';
 import {
   InvalidParamError,
   MissingParamError,
@@ -10,8 +11,11 @@ import { EmailValidator } from '../signup/signup-protocols';
 export class LoginController implements Controller {
   private readonly emailValidator: EmailValidator;
 
-  constructor(emailValidator: EmailValidator) {
+  private readonly authentication: Authentication;
+
+  constructor(emailValidator: EmailValidator, authentication: Authentication) {
     this.emailValidator = emailValidator;
+    this.authentication = authentication;
   }
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -36,6 +40,8 @@ export class LoginController implements Controller {
           resolve(badRequest(new InvalidParamError('email'))),
         );
       }
+
+      await this.authentication.auth(email, password);
 
       return null;
     } catch (error) {
