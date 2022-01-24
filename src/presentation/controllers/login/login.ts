928c4784a855bd439fd07616a96a629b45f10c78
Rafael Silva Menezes <rafasilva320.rs@gmail.com>
2022-01-24 05:47:01 -0300

    feat: ensure LoginController returns 500 EmailValidator throws

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,5 +1,9 @@
-import { InvalidParamError, MissingParamError } from '../../errors';
-import { badRequest } from '../../helpers';
+import {
+  InvalidParamError,
+  MissingParamError,
+  ServerError,
+} from '../../errors';
+import { badRequest, serverError } from '../../helpers';
 import { Controller, HttpRequest, HttpResponse } from '../../protocols';
 import { EmailValidator } from '../signup/signup-protocols';
 
@@ -11,27 +15,31 @@ export class LoginController implements Controller {
   }
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { email, password } = httpRequest.body;
+    try {
+      const { email, password } = httpRequest.body;
 
-    if (!httpRequest.body.email) {
-      return new Promise(resolve =>
-        resolve(badRequest(new MissingParamError('email'))),
-      );
+      if (!httpRequest.body.email) {
+        return new Promise(resolve =>
+          resolve(badRequest(new MissingParamError('email'))),
+        );
+      }
+
+      if (!httpRequest.body.password) {
+        return new Promise(resolve =>
+          resolve(badRequest(new MissingParamError('password'))),
+        );
+      }
+
+      const isValid = this.emailValidator.isValid(httpRequest.body.email);
+      if (!isValid) {
+        return new Promise(resolve =>
+          resolve(badRequest(new InvalidParamError('email'))),
+        );
+      }
+
+      return null;
+    } catch (error) {
+      return serverError(error);
     }
-
-    if (!httpRequest.body.password) {
-      return new Promise(resolve =>
-        resolve(badRequest(new MissingParamError('password'))),
-      );
-    }
-
-    const isValid = this.emailValidator.isValid(httpRequest.body.email);
-    if (!isValid) {
-      return new Promise(resolve =>
-        resolve(badRequest(new InvalidParamError('email'))),
-      );
-    }
-
-    return null;
   }
 }
