f45cad80405aba6718e9ba000c121193aa964ab1
Rafael Silva Menezes <rafasilva320.rs@gmail.com>
2022-01-25 07:26:51 -0300

    refactor: move compare field validation to the ValidationComposite

--- src/main/factories/signup-validation.spec.ts
+++ src/main/factories/signup-validation.spec.ts
@@ -1,4 +1,5 @@
 import { Validation } from '../../presentation/helpers';
+import { CompareFieldsValidation } from '../../presentation/helpers/validators/compare-fields-validation';
 import { RequiredFieldValidation } from '../../presentation/helpers/validators/required-field-validation';
 import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite';
 import { makeSignUpValidation } from './signup-validation';
@@ -12,6 +13,10 @@ describe('SignUpValidation Factory', () => {
     for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
       validations.push(new RequiredFieldValidation(field));
     }
+
+    validations.push(
+      new CompareFieldsValidation('password', 'passwordConfirmation'),
+    );
     expect(ValidationComposite).toHaveBeenCalledWith(validations);
   });
 });
--- src/main/factories/signup-validation.ts
+++ src/main/factories/signup-validation.ts
@@ -1,4 +1,5 @@
 import { Validation } from '../../presentation/helpers';
+import { CompareFieldsValidation } from '../../presentation/helpers/validators/compare-fields-validation';
 import { RequiredFieldValidation } from '../../presentation/helpers/validators/required-field-validation';
 import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite';
 
@@ -7,5 +8,9 @@ export const makeSignUpValidation = (): Validation => {
   for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
     validations.push(new RequiredFieldValidation(field));
   }
+
+  validations.push(
+    new CompareFieldsValidation('password', 'passwordConfirmation'),
+  );
   return new ValidationComposite(validations);
 };
